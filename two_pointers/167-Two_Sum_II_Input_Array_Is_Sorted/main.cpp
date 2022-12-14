92d7599cf520bbf2edbc40250a254f88c40c910d
Minh Bui <ultraminh2709@gmail.com>
2022-12-13 23:11:16 -0800

    12/13/22 - 167-Two Sum II, Input Array Is Sorted - Medium - Two Pointers

---
+++ two_pointers/167-Two_Sum_II_Input_Array_Is_Sorted/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/* This is leetcode problem 167 - Two Sum II, Input Array Is Sorted
+ * Difficulty: Medium. Type: Medium
+ * Goal: Given an input array of non-decreasing integers and a target, find the index of two indices where the values 
+ * add up to be equal to the target. It is guaranteed to have only one solution.
+ * Approach: Since the array is sorted, place two pointers at the start and end.
+ * If the sum of the two pointers is more than the target, then decrement the j pointer, this decreases the current sum
+ * If the sum is less than the target, then increment the i pointer, this increases the current sum.
+ * i++ and j-- until we reach the end, that is i = j. If at any point i + j = target, then return and break. If not, then
+ * an empty vector will be returned.
+*/
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& numbers, int target) {
+		vector<int> output;
+		int i = 0, j = numbers.size() - 1;
+		while(i < j) {
+			cout << "num[i]: " << numbers[i] << " num[j]: " << numbers[j] << endl;
+			if(numbers[j] + numbers[i] > target) j--;
+			else if(numbers[j] + numbers[i] < target) i++;
+			else {
+				output.push_back(i + 1);
+				output.push_back(j + 1);
+				break;
+			}
+		}
+		return output;
+	}
+};
+int main ()
+{
+	vector<int> input = {2,7,11,15};
+	int target;
+	cin >> target;
+	Solution s;	
+	vector<int> output = s.twoSum(input, target);
+	for(int i : output) cout << i << endl;
+	return 0;
+}
