440b56021e77a2bc6d7c944a69ee69db978a8a41
Minh Bui <ultraminh2709@gmail.com>
2022-12-14 02:32:26 -0800

    added some changes to the rust program

--- two_pointers/167-Two_Sum_II_Input_Array_Is_Sorted/test.rs
+++ two_pointers/167-Two_Sum_II_Input_Array_Is_Sorted/test.rs
@@ -7,12 +7,8 @@ impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
         let (mut i,mut j) = (0, numbers.len() -1 );
         while i < j {
-            if numbers[i] + numbers[j] > target {
-                j -= 1;
-            } 
-            else if numbers[i] + numbers[j] < target {
-                i += 1;
-            }
+            if numbers[i] + numbers[j] > target { j -= 1; } 
+            else if numbers[i] + numbers[j] < target { i += 1; }
             else { return vec![(i as i32)+1,(j as i32)+1]; }
         }
         return vec![];
@@ -23,5 +19,4 @@ fn main() {
     let numbers = vec![2,7,11,15];
     let output = Solution::two_sum(numbers,target);
     println!("{:?}",output);
-
 }
